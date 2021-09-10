96dd0fc4445499e4980722004232e54d7561eae6
Honey <yurjune@naver.com>
2021-09-11 04:27:07 +0900

    ch-4-12 commit infinite scrolling

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import AppLayout from '../components/AppLayout';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import AppLayout from '../components/AppLayout';
 import PostCard from '../components/PostCard';
 import PostForm from '../components/PostForm';
+import { LOAD_POSTS_REQUEST } from '../reducers/post';
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const { hasMorePosts, loadPostsLoading } = useSelector((state) => state.post);
   const { me } = useSelector((state) => state.user);
   const { mainPosts } = useSelector((state) => state.post);
+
+  useEffect(() => {
+    dispatch({
+      type: LOAD_POSTS_REQUEST,
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log('useEffect implemented');
+    function onScroll() {
+      // console.log(
+      //   window.scrollY, // 문서가 얼마나 스크롤됬는지
+      //   document.documentElement.clientHeight, // 화면에 보이는 높이
+      //   document.documentElement.scrollHeight, // 요소의 전체 높이
+      // );
+      console.log('scrolling now');
+      if (window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300) {
+        if (hasMorePosts && !loadPostsLoading) {
+          dispatch({
+            type: LOAD_POSTS_REQUEST,
+          });
+        }
+      }
+    }
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      console.log('eventListener removed');
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [hasMorePosts, loadPostsLoading]);
 
   return (
     <AppLayout>
--- front/sagas/post.js
+++ front/sagas/post.js
@@ -1,13 +1,34 @@
-import { all, fork, takeLatest, put, delay } from 'redux-saga/effects';
+import { all, fork, takeLatest, put, delay, throttle } from 'redux-saga/effects';
 import axios from 'axios';
 import shortId from 'shortid';
 
 import {
   ADD_POST_REQUEST, ADD_POST_SUCCESS, ADD_POST_FAILURE,
+  generateDummyPost, LOAD_POSTS_REQUEST, LOAD_POSTS_SUCCESS, LOAD_POSTS_FAILURE,
   REMOVE_POST_REQUEST, REMOVE_POST_SUCCESS, REMOVE_POST_FAILURE,
   ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE,
   ADD_POST_TO_ME, REMOVE_POST_OF_ME,
 } from '../reducers/post';
+
+function loadPostsAPI(data) {
+  return axios.get('/api/posts', data);
+}
+
+function* loadPosts() {
+  try {
+    // const result = yield call(loadPostsAPI, action.data);
+    yield delay(1000);
+    yield put({
+      type: LOAD_POSTS_SUCCESS,
+      data: generateDummyPost(10),
+    });
+  } catch (err) {
+    yield put({
+      type: LOAD_POSTS_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
 
 function addPostAPI(data) {
   return axios.post('/api/post', data);
@@ -81,6 +102,10 @@ function* addComment(action) {
   }
 }
 
+function* watchLoadPosts() {
+  yield throttle(5000, LOAD_POSTS_REQUEST, loadPosts);
+}
+
 function* watchAddPost() {
   yield takeLatest(ADD_POST_REQUEST, addPost);
 }
@@ -95,6 +120,7 @@ function* watchAddComment() {
 
 export default function* postSaga() {
   yield all([
+    fork(watchLoadPosts),
     fork(watchAddPost),
     fork(watchRemovePost),
     fork(watchAddComment),
